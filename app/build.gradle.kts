fde2e15744f0c44edcf09e8d1cc03ecc51dd6d42
dattuanh <hdat1216@gmail.com>
2024-07-05 15:18:04 +0700

    add plugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,6 +36,7 @@ android {
 
 dependencies {
     implementation ("com.github.bumptech.glide:glide:4.12.0")
+    implementation(libs.gms.play.services.maps)
     annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
     implementation ("com.squareup.retrofit2:retrofit:2.0.2")
     implementation ("com.squareup.retrofit2:converter-gson:2.0.2")
